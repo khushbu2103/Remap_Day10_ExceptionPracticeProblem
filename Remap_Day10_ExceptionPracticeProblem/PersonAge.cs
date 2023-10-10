ab6b3c32cda506e52ec21a418bcc1499cd115b10
khushbu <khushbusoni475@gmail.com>
2023-10-10 23:21:51 +0530

    Adding exception practice problem

---
+++ Remap_Day10_ExceptionPracticeProblem/PersonAge.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remap_Day10_ExceptionPracticeProblem
+{
+    internal class PersonAge
+    {
+        public static void PersonAgeAbility(string Age)
+        {
+           
+            try
+            {
+                int age = int.Parse(Age);
+
+                if (age >= 1 && age <= 14)
+                    Console.WriteLine("Children");
+                else if (age > 14 && age <= 24)
+                    Console.WriteLine("Youth");
+                else if (age > 24 && age <= 64)
+                    Console.WriteLine("Adults");
+                else if (age > 64)
+                    Console.WriteLine("Seniors");
+                else
+                    throw new CustomAgeException("Invalid Age");
+            }
+            //catch (FormatException ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            catch (CustomAgeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Remap_Day10_ExceptionPracticeProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Remap_Day10_ExceptionPracticeProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a person's age:");
+            string age = Console.ReadLine();
+           
+            PersonAge.PersonAgeAbility(age);
+            Console.ReadLine();
+        }
+    }
+}
